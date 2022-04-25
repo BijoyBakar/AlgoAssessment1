caef89528fd04f42f8ff013fc05e44cdab7ec176
bijoybakar <bakarbijoy@gmail.com>
2022-04-25 01:56:20 +0100

    linear search works even if no number is found

--- LinearSearch.cs
+++ LinearSearch.cs
@@ -7,17 +7,35 @@ public class LinearSearch
         int index = 0;
         int variance = list[0];
         int List_count = list.Count;
-
+        int count = 0;
         for (int x=0; x < List_count; x++)
         {
             int subtract = value - list[x];
             int i = Math.Abs(subtract);
+            
             if (i <= variance)
             {
+                
                 variance = Math.Abs(subtract);
                 index = x;
+                count++;
+
             }
+            else if (value > list[x])
+            {
+                Math.Abs(value + list[1]);
+                index = x + 1;
+            }
+
+
         }
+        Console.WriteLine("The number entered appears "+count+" times");
+        if (count == 0)
+        {
+            Console.WriteLine("The closest index are: ");
+            
+        }
+        
         return index;
     }
 }
